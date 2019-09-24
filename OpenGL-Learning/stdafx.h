d0ee188db0505e9552e0af0205ffed93d12e030a
insoLLLent <vitaly@lifanoff.ru>
2019-09-24 21:48:56 +0300

    included libs

--- OpenGL-Learning/stdafx.h
+++ OpenGL-Learning/stdafx.h
@@ -1,7 +1,36 @@
 #pragma once
 
+#ifndef STDAFX_H
+#define STDAFX_H
+
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
-#include <iostream>
 #include <cstdlib>
+#include <cstdint>
+#include <cstddef> // for offsetof
+#include <cmath>
+#include <exception>
+#include <type_traits>
+#include <memory>
+#include <iterator>
+#include <functional>
+#include <utility>
+#include <algorithm>
+#include <numeric>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <filesystem>
+#include <chrono>
+#include <thread>
+#include <random>
+
+#endif // !STDAFX_H
