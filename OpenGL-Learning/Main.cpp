b7dc7dc82cfe6db07b7447b1cba17c4052a4e303
insoLLLent <vitaly@lifanoff.ru>
2019-09-30 18:58:09 +0300

    removed files

--- OpenGL-Learning/Main.cpp
+++ OpenGL-Learning/Main.cpp
@@ -1,13 +1,5 @@
 #include "stdafx.h"
 
-#include "Application.h"
-
 int main(int argc, char** argv) {
-	try {
-		Application::GetInstance()->Run();
-	} catch (...) {
-		return EXIT_FAILURE;
-	}
-
 	return EXIT_SUCCESS;
 }
