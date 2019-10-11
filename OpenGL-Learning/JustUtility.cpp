30cb89394ec8378a76977fa697df784186f85c38
insoLLLent <vitaly@lifanoff.ru>
2019-10-11 15:46:05 +0300

    updated JustUtility

--- OpenGL-Learning/JustUtility.cpp
+++ OpenGL-Learning/JustUtility.cpp
@@ -1,2 +1,15 @@
 #include "stdafx.h"
 #include "JustUtility.h"
+
+namespace JustUtility {
+
+ScreenSize::ScreenSize(int x) :
+	width(x), height(x) {}
+
+ScreenSize::ScreenSize(int w, int h) :
+	width(w), height(h) {}
+
+Color::Color(GLfloat r, GLfloat g, GLfloat b, GLfloat a) :
+	r(r), g(g), b(b), a(a) {}
+
+} // namespace JustUtility
--- OpenGL-Learning/JustUtility.h
+++ OpenGL-Learning/JustUtility.h
@@ -5,6 +5,27 @@
 
 namespace JustUtility {
 
+#pragma region Structures
+
+struct ScreenSize {
+	int width = 0;
+	int height = 0;
+
+	explicit ScreenSize(int x);
+	explicit ScreenSize(int w, int h);
+};
+
+struct Color {
+	GLfloat r = 0.0f;
+	GLfloat g = 0.0f;
+	GLfloat b = 0.0f;
+	GLfloat a = 1.0f;
+
+	explicit Color(GLfloat r, GLfloat g, GLfloat b, GLfloat a);
+};
+
+#pragma endregion
+
 enum AttribIndex : GLuint {
 	POSITION,
 	TEXTURE,
