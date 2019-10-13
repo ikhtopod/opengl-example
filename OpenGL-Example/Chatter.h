78ffc24fb17f4300a0eaf4e84a1387906c6dd8e5
insoLLLent <vitaly@lifanoff.ru>
2019-10-13 17:57:24 +0300

    returned old Chatter

--- OpenGL-Example/Chatter.h
+++ OpenGL-Example/Chatter.h
@@ -3,48 +3,22 @@
 #ifndef CHATTER_H
 #define CHATTER_H
 
-class Chatter final {
-private:
-	static std::stringstream s_stink;
+namespace Chatter {
 
-private:
-	Chatter() = default;
-	explicit Chatter(const Chatter&) = delete;
-	explicit Chatter(Chatter&&) = delete;
-	Chatter& operator=(const Chatter&) = delete;
-	Chatter& operator=(Chatter&&) = delete;
+void Say();
 
-private:
-	static void _Stink();
+template <typename T, typename... Args>
+void Say(T val, Args... args) {
+	std::cerr << val << ' ';
+	Chatter::Say(args...);
+}
 
-	template <typename T, typename... Args>
-	static void _Stink(T val, Args... args) {
-		s_stink << val << ' ';
-		Chatter::_Stink(args...);
-	}
+template <typename T, typename... Args>
+void Stink(T val, Args... args) {
+	std::cerr << "Error: ";
+	Chatter::Say(val, args...);
+}
 
-public:
-	~Chatter() = default;
-
-public:
-	static std::string LastStink();
-	static const char* c_LastStink();
-
-	static void Say();
-
-public:
-	template <typename T, typename... Args>
-	static void Say(T val, Args... args) {
-		std::cerr << val << ' ';
-		Chatter::Say(args...);
-	}
-
-	template <typename T, typename... Args>
-	static void Stink(T val, Args... args) {
-		s_stink.str("");
-		std::cerr << "Error: ";
-		Chatter::_Stink(val, args...);
-	}
-};
+}; // namespace Chatter
 
 #endif // !CHATTER_H
